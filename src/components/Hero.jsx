4a18cbcf37b9306c029562511c9054a43c026fc9
Yogina Khandelwal <yogina.khandelwal@gmail.com>
2024-12-28 04:12:12 -0500

    created more components and used props to get data

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Hero = () => {
+const Hero = ({title = 'Become a React Dev', subtitle='Find the React job that fits your skills and needs'}) => {
   return (
     <>
       <section className="bg-indigo-700 py-20 mb-4">
@@ -11,10 +11,10 @@ const Hero = () => {
           <h1
             className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
           >
-            Become a React Dev
+            {title}
           </h1>
           <p className="my-4 text-xl text-white">
-            Find the React job that fits your skills and needs
+            {subtitle}
           </p>
         </div>
       </div>
