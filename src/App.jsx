389df05ad58f6c8bd2689d46a6ca0821e523ce23
Yogina Khandelwal <yogina.khandelwal@gmail.com>
2024-12-28 05:43:41 -0500

    completed home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
 import JobListings from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
 
 const App = () => {
   return (
@@ -11,19 +12,10 @@ const App = () => {
     {/* <!-- Hero --> */}
     <Hero />
     {/* <!-- Developers and Employers --> */}
-    <HomeCards />
-   
-
+    <HomeCards />  
     {/* <!-- Browse Jobs --> */}
     <JobListings />
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+    <ViewAllJobs />    
     </>
   );
 };
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
+import { FaMapMarker} from 'react-icons/fa';
 
 const JobListing = ({job}) => {
+    const [ showFullDescription, setShowFullDescription] = useState(false);
+    let description = job.description;
+    if(!showFullDescription){
+        description = description.substring(0,90) + '...';
+    }
   return (
     <>
        <div className="bg-white rounded-xl shadow-md relative">
@@ -11,8 +18,10 @@ const JobListing = ({job}) => {
                   </div>
     
                   <div className="mb-5">
-                  {job.description}
+                  {description}
                   </div>
+                  <button onClick={()=>setShowFullDescription((prevState)=> !prevState)}
+                  className='text-indigo-500 mb-5 hover:text-indigo-60'>{showFullDescription?'Less':'More'}</button>
     
                   <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
     
@@ -20,7 +29,7 @@ const JobListing = ({job}) => {
     
                   <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
-                      <i className="fa-solid fa-location-dot text-lg"></i>
+                      <FaMapMarker className='inline text-lg mb-1 mr-1'/>
                       {job.location}
                     </div>
                     <a
