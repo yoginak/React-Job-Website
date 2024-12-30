68af8cf14bdc1f3a904e68cddba8c908ab69d675
Yogina Khandelwal <yogina.khandelwal@gmail.com>
2024-12-30 01:40:01 -0500

    completed job page setup

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import JobListings from '../components/JobListings'
 
 const JobsPage = () => {
   return (
-    <>
-      <h1>JOBSSSSSSS</h1>
-    </>
+    <section className='bg-blue-50 px-4 py-6'>
+      <JobListings/>
+    </section>
   )
 }
 
