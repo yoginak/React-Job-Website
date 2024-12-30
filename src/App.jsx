4babb368c6ab845f6fab3289482a20e153190b4b
Yogina Khandelwal <yogina.khandelwal@gmail.com>
2024-12-30 00:45:35 -0500

    initial routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />, // Main layout wrapper
+    children: [
+      {
+        path: "/", // Nested route
+        element: <HomePage />, // Content for this route
+      },
+    ],
+  },
+]);
 
 const App = () => {
   return (
-    <>
-    <Navbar />   
-    {/* <!-- Hero --> */}
-    <Hero />
-    {/* <!-- Developers and Employers --> */}
-    <HomeCards />  
-    {/* <!-- Browse Jobs --> */}
-    <JobListings />
-    <ViewAllJobs />    
-    </>
+    <RouterProvider router={router} />
   );
 };
 
-export default App
+export default App;
